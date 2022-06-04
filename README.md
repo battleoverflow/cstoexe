db14e2f2a821c8bf406cb1f08ec5fdfbc88f90d5
Hifumi <hifumisecurity@protonmail.com>
2022-06-03 22:30:05 -0500

    Cleaned up entire project a little bit

--- README.md
+++ README.md
@@ -1,11 +1,17 @@
-# C# => .exe
+# C# => Executable
 Simple bash script for converting a C# file to an executable.
 
 ## Usage
+This command will give the script executable permissions
 ```bash
 chmod +x cstoexe.sh
 ```
 
+Run the script
 ```bash
 ./cstoexe.sh
 ```
+
+Tested on:
+- Linux (Debian)
+- macOS
--- cstoexe.sh
+++ cstoexe.sh
@@ -1,51 +1,62 @@
 #!/bin/bash
 
 #########################################################
-# Creator: Hifumi1337                                   #
-# Created: 09/16/2021                                   #
-# Version: 1.0.2                                        #
+# Author: Hifumi1337                                    #
+# Version: 1.0.3                                        #
 # Project: https://github.com/Hifumi1337/cstoexe        #
+# Created: 09/16/2021                                   #
 #########################################################
 
-echo
-echo "Creator: Hifumi1337 (https://github.com/Hifumi1337)"
-echo "Version: 1.0.2"
-echo "Project: https://github.com/Hifumi1337/cstoexe"
-echo "Support the dev: https://github.com/sponsors/hifumi1337"
-echo
+function banner {
+    echo
+    echo "Author: Hifumi1337 (https://github.com/Hifumi1337)"
+    echo "Version: 1.0.3"
+    echo "Project: https://github.com/Hifumi1337/cstoexe"
+    echo
+}
 
 # Must have sudo permissions for a clean install
 
-# Updates current packages
-sudo apt update
+function cstoexe {
 
-# Installs required package
-if ! which mono > /dev/null; then
-    echo -e "Command is not present. Would you like to install it (y/n)? \c"
-    read user_reply
-    
-    if "$user_reply" = "y"; then
-        mono_install = "sudo apt install mono-complete"
-        echo $mono_install
+    banner
+
+    # Installs required package
+    if ! which mono > /dev/null; then
+        echo -e "Command is not present. Would you like to install it (y/n)? \c"
+        read user_reply
+        
+        if "$user_reply" = "y"; then
+            # Updates current packages
+            sudo apt update
+
+            echo
+
+            mono_install = "sudo apt install mono-complete"
+            echo $mono_install
+        fi
+
+    else
+        echo "Mono is already installed"
+        echo
     fi
 
-else
-    echo "Mono is already installed."
-fi
+    echo -n "Input the name of your C# file without the extension (ex: fileName): "
+    read csFileName
 
-echo -n "Input the name of your C# file without the extension (ex: fileName): "
-read csFileName
+    # Converts C# script to an executable 
+    mcs -out:$csFileName $csFileName.cs
 
-# Converts C# script to an executable 
-mcs -out:$csFileName.exe $csFileName.cs
+    echo -n "Would you like to run your new executable (y/n): "
+    read answer
 
-echo -n "Would you like to run your new executable (y/n): "
-read answer
+    if [ "$answer" == "y" ]
+    then
+        # Runs/prints generated exe file
+        mono $csFileName
+    else
+        echo "Thank you for using cstoexe!"
+    fi   
+}
 
-if [ "$answer" == "y" ]
-then
-    # Runs/prints generated exe file
-    mono $csFileName.exe
-else
-    echo "Thank you for using cstoexe!"
-fi
+cstoexe
